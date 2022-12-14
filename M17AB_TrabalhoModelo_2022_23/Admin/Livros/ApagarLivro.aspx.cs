0763e7a8e97cda3e91f8a77b6200c9f3dababcfb
alunosNET <alunosnet@gmail.com>
2022-12-14 22:40:46 +0000

    Inserção e Listagem de livros

---
+++ M17AB_TrabalhoModelo_2022_23/Admin/Livros/ApagarLivro.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M17AB_TrabalhoModelo_2022_23.Admin.Livros
+{
+    public partial class ApagarLivro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //TODO: Validar sessão
+
+            //querystring nlivro
+
+        }
+    }
+}
--- M17AB_TrabalhoModelo_2022_23/Admin/Livros/Livros.aspx.cs
+++ M17AB_TrabalhoModelo_2022_23/Admin/Livros/Livros.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using M17AB_TrabalhoModelo_202223.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,11 +15,29 @@ namespace M17AB_TrabalhoModelo_2022_23.Admin.Livros
         {
             //Validar a sessão
 
+            ConfigurarGrid();
+
             if (!IsPostBack)
             {
                 AtualizarGrid();
             }
         }
+        /// <summary>
+        /// Configuração da grelha dos livros
+        /// </summary>
+        private void ConfigurarGrid()
+        {
+            gvLivros.AllowPaging= true;
+            gvLivros.PageSize = 5;
+            gvLivros.PageIndexChanging += GvLivros_PageIndexChanging;
+        }
+
+        private void GvLivros_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvLivros.PageIndex = e.NewPageIndex;
+            AtualizarGrid();
+        }
+
         /// <summary>
         /// Adicionar umlivro
         /// </summary>
@@ -95,8 +114,96 @@ namespace M17AB_TrabalhoModelo_2022_23.Admin.Livros
         /// </summary>
         private void AtualizarGrid()
         {
+           /* 
             Livro lv = new Livro();
             gvLivros.DataSource = lv.ListaTodosLivros();
+            gvLivros.DataBind();
+           */
+            gvLivros.Columns.Clear();
+            Livro lv=new Livro();
+            DataTable dados = lv.ListaTodosLivros();
+
+            DataColumn dcEditar = new DataColumn();
+            dcEditar.ColumnName = "Editar";
+            dcEditar.DataType = Type.GetType("System.String");
+            dados.Columns.Add(dcEditar);
+
+            DataColumn dcApagar = new DataColumn();
+            dcApagar.ColumnName = "Apagar";
+            dcApagar.DataType = Type.GetType("System.String");
+            dados.Columns.Add(dcApagar);
+
+            //colunas da gridview
+            gvLivros.DataSource= dados;
+            gvLivros.AutoGenerateColumns = false;
+
+            //Editar
+            HyperLinkField hlEditar= new HyperLinkField();
+            hlEditar.HeaderText = "Editar";
+            hlEditar.DataTextField = "Editar";
+            hlEditar.Text = "Editar...";
+            hlEditar.DataNavigateUrlFormatString = "EditarLivro.aspx?nlivro={0}";
+            hlEditar.DataNavigateUrlFields = new string[] { "nlivro" };
+            gvLivros.Columns.Add(hlEditar);
+            //Apagar
+            HyperLinkField hlApagar = new HyperLinkField();
+            hlApagar.HeaderText = "Apagar";
+            hlApagar.DataTextField = "Apagar";
+            hlApagar.Text = "Apagar...";
+            hlApagar.DataNavigateUrlFormatString = "ApagarLivro.aspx?nlivro={0}";
+            hlApagar.DataNavigateUrlFields = new string[] { "nlivro" };
+            gvLivros.Columns.Add(hlApagar);
+            //nlivro
+            BoundField bfnlivro= new BoundField();
+            bfnlivro.HeaderText = "Nº Livro";
+            bfnlivro.DataField = "nlivro";
+            gvLivros.Columns.Add(bfnlivro);
+            //nome
+            BoundField bfnome=new BoundField();
+            bfnome.HeaderText = "Nome";
+            bfnome.DataField = "nome";
+            gvLivros.Columns.Add(bfnome);
+            //ano
+            BoundField bfano = new BoundField();
+            bfano.HeaderText = "Ano";
+            bfano.DataField = "ano";
+            gvLivros.Columns.Add(bfano);
+            //data aquisição
+            BoundField bfdata=new BoundField();
+            bfdata.HeaderText = "Data aquisição";
+            bfdata.DataField = "data_aquisicao";
+            bfdata.DataFormatString = "{0:dd-MM-yyyy}";
+            gvLivros.Columns.Add(bfdata);
+            //Preço
+            BoundField bfpreco=new BoundField();
+            bfpreco.HeaderText = "Preço";
+            bfpreco.DataField = "preco";
+            bfpreco.DataFormatString = "{0:C}";
+            gvLivros.Columns.Add(bfpreco);
+            //Autor
+            BoundField bfautor = new BoundField();
+            bfautor.HeaderText = "Autor";
+            bfautor.DataField = "autor";
+            gvLivros.Columns.Add(bfautor);
+            //Tipo
+            BoundField bftipo = new BoundField();
+            bftipo.HeaderText = "Tipo";
+            bftipo.DataField = "tipo";
+            gvLivros.Columns.Add(bftipo);
+            //Estado
+            BoundField bfestado=new BoundField();
+            bfestado.HeaderText = "Estado";
+            bfestado.DataField = "estado";
+            gvLivros.Columns.Add(bfestado);
+            //Capa
+            ImageField ifcapa=new ImageField();
+            ifcapa.HeaderText = "Capa";
+            int aleatorio = new Random().Next(99999);
+            ifcapa.DataImageUrlFormatString = "~/Public/Imagens/{0}.jpg?"+aleatorio;
+            ifcapa.DataImageUrlField = "nlivro";
+            ifcapa.ControlStyle.Width = 200;
+            gvLivros.Columns.Add(ifcapa);
+
             gvLivros.DataBind();
         }
     }
